1a64b53340f7f4d151ba5ef7c9355e2fb563abe5
unknown <valerii.shvydkoi@hneu.net>
2026-04-30 00:42:49 +0300

    feat(models): додавання моделі Pet з валідаторами шкал та м'якого видалення

---
+++ backend/core/models/pet.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from .base import SoftDeleteModel, TimeStampedModel
+from .shelter import Shelter
+
+
+class Pet(SoftDeleteModel, TimeStampedModel):
+    name = models.CharField(max_length=100)
+    shelter = models.ForeignKey(Shelter, on_delete=models.CASCADE, related_name='pets')
+    is_available = models.BooleanField(default=True)
+
+    # Шкали 1-5 для алгоритму AHP
+    activity_level = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    sociability = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    stress_resistance = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    # Фізичні параметри для жорстких обмежень
+    weight = models.DecimalField(max_digits=5, decimal_places=2)  # у кг
+
+    def __str__(self):
+        return self.name
