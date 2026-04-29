6b4fd1ff91f95633ccaa70429d5de5d5ca6efc6d
unknown <valerii.shvydkoi@hneu.net>
2026-04-30 00:41:19 +0300

    feat(models): додавання моделі Shelter

---
+++ backend/core/models/shelter.py
@@ -0,0 +1,10 @@
+from django.db import models
+from .base import TimeStampedModel
+
+class Shelter(TimeStampedModel):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
