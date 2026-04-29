55e77361d6ba9570b7bae4edbd75e08dd0146a56
unknown <valerii.shvydkoi@hneu.net>
2026-04-30 00:43:11 +0300

    feat(models): додавання моделі Questionnaire для збереження матриці у JSON

---
+++ backend/core/models/questionnaire.py
@@ -0,0 +1,7 @@
+from django.db import models
+from .base import TimeStampedModel
+from .user import User
+
+class Questionnaire(TimeStampedModel):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='questionnaire')
+    matrix_data = models.JSONField(help_text="Збережена матриця порівнянь AHP")
