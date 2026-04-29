950137312c5e4da6c47dc1affdd35c8f1b5e5d2c
unknown <valerii.shvydkoi@hneu.net>
2026-04-30 00:43:38 +0300

    feat(models): додавання моделі QuestionnaireResult з підтримкою знімка

---
+++ backend/core/models/result.py
@@ -0,0 +1,7 @@
+from django.db import models
+from .base import TimeStampedModel
+from .questionnaire import Questionnaire
+
+class QuestionnaireResult(TimeStampedModel):
+    questionnaire = models.ForeignKey(Questionnaire, on_delete=models.CASCADE, related_name='results')
+    snapshot_data = models.JSONField(help_text="Збережені результати на момент розрахунку")
