d473a343ce7a6491000e3a271532c5e786910a59
unknown <valerii.shvydkoi@hneu.net>
2026-04-30 21:05:41 +0300

    feat(exceptions): додавання класів помилок та DRF обробника для формату {status, code, detail}

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -147,3 +147,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Налаштування Django REST Framework
+REST_FRAMEWORK = {
+    'EXCEPTION_HANDLER': 'core.exceptions.custom_exception_handler',
+}
+
+# Підключення логування
+from core.logging_config import LOGGING
