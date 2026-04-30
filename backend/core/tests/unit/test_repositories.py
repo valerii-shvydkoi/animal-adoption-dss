e4d393cd497b5e854ed90bbe6e4949084a8ff254
unknown <valerii.shvydkoi@hneu.net>
2026-04-30 21:45:54 +0300

    test(repositories): додавання модульних тестів для методів репозиторіїв

---
+++ backend/core/tests/unit/test_repositories.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from core.models import Pet, Shelter
+from core.repositories.pet_repository import PetRepository
+
+class PetRepositoryTest(TestCase):
+    def setUp(self):
+        self.shelter = Shelter.objects.create(name="Притулок", address="Адреса", phone="123")
+        self.available_pet = Pet.objects.create(
+            name="Доступний", shelter=self.shelter, is_available=True,
+            activity_level=3, sociability=3, stress_resistance=3, weight=10.0
+        )
+        self.unavailable_pet = Pet.objects.create(
+            name="Зайнятий", shelter=self.shelter, is_available=False,
+            activity_level=3, sociability=3, stress_resistance=3, weight=10.0
+        )
+
+    def test_get_available_excludes_unavailable(self):
+        pets = PetRepository.get_available()
+        self.assertIn(self.available_pet, pets)
+        self.assertNotIn(self.unavailable_pet, pets)
