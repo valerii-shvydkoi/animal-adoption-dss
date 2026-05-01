68fa4a4d4504e99bd89290cdaf363f9996cea0fd
unknown <valerii.shvydkoi@hneu.net>
2026-05-01 17:49:14 +0300

    test(security): додавання тестів обмеження частоти запитів для захисту від брутфорсу

---
+++ backend/core/tests/unit/test_throttling.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+from django.urls import reverse
+
+
+class ThrottlingTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.url = reverse('token_obtain_pair')
+
+    def test_login_throttling(self):
+        # Робимо 5 хибних запитів (максимальний ліміт)
+        for _ in range(5):
+            self.client.post(self.url, {'email': 'test@test.com', 'password': 'wrong'})
+
+        # 6-й запит має бути заблокований системою (код 429 Too Many Requests)
+        response = self.client.post(self.url, {'email': 'test@test.com', 'password': 'wrong'})
+        self.assertEqual(response.status_code, 429)
