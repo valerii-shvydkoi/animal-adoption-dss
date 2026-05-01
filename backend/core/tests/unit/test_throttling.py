4d7cbc970da14c7e72d58aa2728160be16dbcff3
unknown <valerii.shvydkoi@hneu.net>
2026-05-01 18:05:26 +0300

    chore(lint): додавання порожнього рядка в кінці файлу test_throttling.py

--- backend/core/tests/unit/test_throttling.py
+++ backend/core/tests/unit/test_throttling.py
@@ -6,13 +6,15 @@ from django.urls import reverse
 class ThrottlingTest(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.url = reverse('token_obtain_pair')
+        self.url = reverse("token_obtain_pair")
 
     def test_login_throttling(self):
         # Робимо 10 хибних запитів (максимальний ліміт для anon)
         for _ in range(10):
-            self.client.post(self.url, {'email': 'test@test.com', 'password': 'wrong'})
+            self.client.post(self.url, {"email": "test@test.com", "password": "wrong"})
 
         # 11-й запит має бути заблокований системою (код 429 Too Many Requests)
-        response = self.client.post(self.url, {'email': 'test@test.com', 'password': 'wrong'})
+        response = self.client.post(
+            self.url, {"email": "test@test.com", "password": "wrong"}
+        )
         self.assertEqual(response.status_code, 429)
